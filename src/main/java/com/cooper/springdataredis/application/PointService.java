bc9878a5679b335a69eb4667e47c7026a5b7bb2a
cooper <pbg0205@naver.com>
2022-03-31 15:04:39 +0900

    first commit

--- src/main/java/com/cooper/springdataredis/application/PointService.java
+++
@@ -1,4 +0,0 @@
-package com.cooper.springdataredis.application;
-
-public interface PointService {
-}
--- src/main/java/com/cooper/springdataredis/domain/PointRedisRepository.java
+++
@@ -1,6 +0,0 @@
-package com.cooper.springdataredis.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface PointRedisRepository extends CrudRepository<Point, String> {
-}
